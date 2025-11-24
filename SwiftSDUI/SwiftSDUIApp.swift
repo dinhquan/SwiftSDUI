b2864aae5d8ad507cdbdcf51dbbe9af9b51f1d42
Quan <quan@tap.pm>
2025-11-24 12:02:53 +0700

    Initial Commit

---
+++ SwiftSDUI/SwiftSDUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSDUIApp.swift
+//  SwiftSDUI
+//
+//  Created by Quan on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSDUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
